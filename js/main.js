95055b1764ab6e5f13b8b6110a2196bcfb636cc5
mayra paez <maypaez_ba_90@hotmail.com>
2020-10-31 15:59:09 -0300

    inicio

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// mi pagina es de cuidado de la piel y mi js es de un test de piel para dar um diagnostico de tipo de piel y puedas armar una rutina de cuidado//
+
+//objetos//
+
+function PielNormal (aspectoPiel, irritacion, brillo, tirante, puntosNegros)
+    this.aspectoPiel =
+    this.irritacion =
+    this.brillo =
+    this.tirante =
+    this.puntosNegros =
+
+function PielMixta (aspectoPiel, irritacion, brillo, tirante, puntosNegros)
+    this.aspectoPiel =
+    this.irritacion =
+    this.brillo =
+    this.tirante =
+    this.puntosNegros =  
+    
+function PielGrasa (aspectoPiel, irritacion, brillo, tirante, puntosNegros)
+    this.aspectoPiel =
+    this.irritacion =
+    this.brillo =
+    this.tirante =
+    this.puntosNegros =   
+    
+function PielSeca (aspectoPiel, irritacion, brillo, tirante, puntosNegros)
+    this.aspectoPiel =
+    this.irritacion =
+    this.brillo =
+    this.tirante =
+    this.puntosNegros =       
